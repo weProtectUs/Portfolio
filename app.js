16985830eff3f15e64ea156b488b4c0f0d713c5b
Your Name <you@example.com>
2023-09-09 14:18:59 -0400

    Improved responsiveness on the webpage

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElement = document.querySelectorAll('.hidden')
 hiddenElement.forEach((el) => observer.observe(el))
 
+function stickyScroll() {
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+} 
+
+window.onscroll = stickyScroll
